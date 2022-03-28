90b853f59aacaad0e77f1c9ee5e9f311913fe671
Matt Gleich <git@mattglei.ch>
2022-03-28 00:52:10 -0400

    feat: clock device
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/clock.rs
+++ src/clock.rs
@@ -1,5 +1,6 @@
 use std::{thread, time::Duration};
 
+use chrono::{Datelike, Local, Timelike};
 use is31fl3731::{devices::CharlieBonnet, IS31FL3731};
 use rppal::{
     hal,
@@ -19,4 +20,69 @@ pub fn setup(i2c: I2c) -> IS31FL3731<i2c::I2c> {
     }
     ic.fill(0, None, 0).expect("Failed to clear display");
     ic
+}
+
+pub fn run(ic: &mut IS31FL3731<i2c::I2c>) {
+    let mut old_ms_time = (1, 1);
+    let mut old_s_time = (1, 1);
+    let mut old_m_time = (1, 1);
+    let mut old_h_time = (1, 1);
+    let mut old_d_time = (1, 1);
+    let mut old_w_time = (1, 1);
+
+    loop {
+        let now = Local::now();
+        old_ms_time = set_pixel(
+            ic,
+            coordinate(
+                (now.timestamp_subsec_micros() % 100000) as f32 / 100_000.0,
+                0,
+            ),
+            old_ms_time,
+        );
+        old_s_time = set_pixel(
+            ic,
+            coordinate(now.timestamp_subsec_millis() as f32 / 1_000.0, 1),
+            old_s_time,
+        );
+        old_m_time = set_pixel(ic, coordinate(now.second() as f32 / 59.0, 2), old_m_time);
+        old_h_time = set_pixel(ic, coordinate(now.minute() as f32 / 59.0, 3), old_h_time);
+        old_d_time = set_pixel(ic, coordinate(now.hour() as f32 / 23.0, 4), old_d_time);
+        old_w_time = set_pixel(
+            ic,
+            coordinate((now.weekday() as usize) as f32 / 6.0, 5),
+            old_w_time,
+        );
+    }
+}
+
+fn coordinate(percent: f32, level: u8) -> (u8, u8) {
+    let mut x = level * 3;
+    let y;
+    let mut loc = (16.0 * percent).round() as u8;
+    if loc == 16 {
+        loc -= 1;
+    }
+    if loc > 7 {
+        x += 1;
+        y = 7 - (loc - 8);
+    } else {
+        y = loc;
+    }
+    (x, y)
+}
+
+fn set_pixel(ic: &mut IS31FL3731<i2c::I2c>, loc: (u8, u8), old_loc: (u8, u8)) -> (u8, u8) {
+    if loc == old_loc {
+        return old_loc;
+    }
+    ic.pixel(old_loc.0, old_loc.1, 1)
+        .expect("Failed to set old pixel location to low");
+    let mut brightness = 10;
+    if loc.1 == 0 {
+        brightness = 30;
+    }
+    ic.pixel(loc.0, loc.1, brightness)
+        .expect("Failed to set new pixel location to high");
+    loc
 }
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ mod clock;
 
 fn main() {
     let i2c = I2c::new().expect("Failed to setup i2c bus");
-    clock::setup(i2c);
+    let mut ic = clock::setup(i2c);
+    clock::run(&mut ic);
 }
