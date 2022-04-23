8a3188a883241946f2de3da674cdc9b124767a02
Matt Gleich <git@mattglei.ch>
2022-04-22 23:22:28 -0400

    feat: back to one
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- Makefile
+++ Makefile
@@ -2,5 +2,5 @@ build-all:
 	cargo build --target=armv7-unknown-linux-gnueabihf
 
 dev-deploy: build-all
-	scp target/armv7-unknown-linux-gnueabihf/debug/mark_1 pi@mgdev.local:~/mark_1
+	scp target/armv7-unknown-linux-gnueabihf/debug/mark_1 pi@marki.local:~/mark_1
 
--- src/clock.rs
+++ src/clock.rs
@@ -7,22 +7,9 @@ use rppal::{
     i2c::{self, I2c},
 };
 
-pub fn setup(i2c: I2c) -> IS31FL3731<i2c::I2c> {
-    let mut delay = hal::Delay;
-    let mut ic = CharlieBonnet::configure(i2c);
-    println!("Animating display on");
-    ic.setup(&mut delay).expect("Failed to setup display");
-    for x in 0..16 {
-        for y in 0..8 {
-            ic.pixel(x, y, 1).expect("Failed to set pixel value");
-            thread::sleep(Duration::from_millis(20));
-        }
-    }
-    ic.fill(0, None, 0).expect("Failed to clear display");
-    ic
-}
+pub fn run() {
+    let mut ic = setup(I2c::with_bus(3).expect("Failed to setup i2c bus"));
 
-pub fn run(ic: &mut IS31FL3731<i2c::I2c>) {
     let mut old_ms_time = (1, 1);
     let mut old_s_time = (1, 1);
     let mut old_m_time = (1, 1);
@@ -33,7 +20,7 @@ pub fn run(ic: &mut IS31FL3731<i2c::I2c>) {
     loop {
         let now = Local::now();
         old_ms_time = set_pixel(
-            ic,
+            &mut ic,
             coordinate(
                 (now.timestamp_subsec_micros() % 100000) as f32 / 100_000.0,
                 0,
@@ -41,15 +28,23 @@ pub fn run(ic: &mut IS31FL3731<i2c::I2c>) {
             old_ms_time,
         );
         old_s_time = set_pixel(
-            ic,
+            &mut ic,
             coordinate(now.timestamp_subsec_millis() as f32 / 1_000.0, 1),
             old_s_time,
         );
-        old_m_time = set_pixel(ic, coordinate(now.second() as f32 / 59.0, 2), old_m_time);
-        old_h_time = set_pixel(ic, coordinate(now.minute() as f32 / 59.0, 3), old_h_time);
-        old_d_time = set_pixel(ic, coordinate(now.hour() as f32 / 23.0, 4), old_d_time);
+        old_m_time = set_pixel(
+            &mut ic,
+            coordinate(now.second() as f32 / 59.0, 2),
+            old_m_time,
+        );
+        old_h_time = set_pixel(
+            &mut ic,
+            coordinate(now.minute() as f32 / 59.0, 3),
+            old_h_time,
+        );
+        old_d_time = set_pixel(&mut ic, coordinate(now.hour() as f32 / 23.0, 4), old_d_time);
         old_w_time = set_pixel(
-            ic,
+            &mut ic,
             coordinate((now.weekday() as usize) as f32 / 6.0, 5),
             old_w_time,
         );
@@ -78,11 +73,26 @@ fn set_pixel(ic: &mut IS31FL3731<i2c::I2c>, loc: (u8, u8), old_loc: (u8, u8)) ->
     }
     ic.pixel(old_loc.0, old_loc.1, 1)
         .expect("Failed to set old pixel location to low");
-    let mut brightness = 10;
+    let mut brightness = 30;
     if loc.1 == 0 {
-        brightness = 30;
+        brightness = 50;
     }
     ic.pixel(loc.0, loc.1, brightness)
         .expect("Failed to set new pixel location to high");
     loc
+}
+
+fn setup(i2c: I2c) -> IS31FL3731<i2c::I2c> {
+    let mut delay = hal::Delay;
+    let mut ic = CharlieBonnet::configure(i2c);
+    println!("Animating display on");
+    ic.setup(&mut delay).expect("Failed to setup display");
+    for x in 0..16 {
+        for y in 0..8 {
+            ic.pixel(x, y, 1).expect("Failed to set pixel value");
+            thread::sleep(Duration::from_millis(20));
+        }
+    }
+    ic.fill(0, None, 0).expect("Failed to clear display");
+    ic
 }
