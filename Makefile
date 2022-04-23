e65aa6bac4b874b3ce3e10990eaa1a7af1309dfb
Matt Gleich <git@mattglei.ch>
2022-04-23 07:11:19 -0400

    feat: eink script
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- Makefile
+++ Makefile
@@ -2,5 +2,6 @@ build-all:
 	cargo build --target=armv7-unknown-linux-gnueabihf
 
 dev-deploy: build-all
+	scp python/eink.py pi@marki.local:~/eink.py
 	scp target/armv7-unknown-linux-gnueabihf/debug/mark_1 pi@marki.local:~/mark_1
 
---
+++ python/eink.py
@@ -0,0 +1,20 @@
+import inky
+from PIL import ImageFont, Image, ImageDraw
+import time
+
+def main():
+    display = inky.auto()
+    x = 0
+    print("Started eink module")
+    while True:
+        out = Image.new("P", (display.WIDTH, display.HEIGHT))
+        fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 20)
+        d = ImageDraw.Draw(out)
+        d.multiline_text((10, 10), f"Hello\nWorld {x}", font=fnt, fill=display.BLACK)
+        display.set_image(out)
+        display.show()
+        time.sleep(10)
+        x += 1
+
+if __name__ == "__main__":
+    main()
